6d824955c84f570d6a6d9398399e52a519251c3d
Elias  Valdez <elias.vc@gmail.com>
2025-08-11 14:40:21 -0400

    Refactored Repeat() into a single line using strings.Repeat(), which improved performance too

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -3,9 +3,5 @@ package iteration
 import "strings"
 
 func Repeat(char string, n int) string {
-	var repeated strings.Builder
-	for range n {
-		repeated.WriteString(char)
-	}
-	return repeated.String()
+	return strings.Repeat(char, n)
 }
