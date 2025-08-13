5db964899a5c10d92ca1584ff0d11f60d6f47ee1
Elias  Valdez <elias.vc@gmail.com>
2025-08-13 11:31:16 -0400

    Added Circle struct, and added Area() methods to both Circle and rectangle. Changed tests to match too

--- structsmethodsinterfaces/shapes.go
+++ structsmethodsinterfaces/shapes.go
@@ -1,8 +1,26 @@
 package structsmethodsinterfaces
 
+import (
+	"math"
+)
+
 type Rectangle struct {
 	Width  float64
 	Height float64
+}
+
+// Area returns the area of a rectangle: r.Height * r.Width
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+type Circle struct {
+	Radius float64
+}
+
+// Area returns the area of a circle: Pi * c.Radius**2
+func (c Circle) Area() float64 {
+	return math.Pi * math.Pow(c.Radius, 2)
 }
 
 // Perimeter calculates the perimeter of a rectangle: 2 * (width + height)
--- structsmethodsinterfaces/shapes_test.go
+++ structsmethodsinterfaces/shapes_test.go
@@ -16,13 +16,25 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{10.0, 10.0}
-	got := Area(rectangle)
-	want := 100.0
+	t.Run("area of a triangle", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
+		want := 100.0
 
-	if got != want {
-		t.Errorf("got %0.1f want %0.1f", got, want)
-	}
+		if got != want {
+			t.Errorf("got %0.1f want %0.1f", got, want)
+		}
+	})
+
+	t.Run("area of a circle", func(t *testing.T) {
+		circle := Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	})
 }
 
 func ExamplePerimeter() {
