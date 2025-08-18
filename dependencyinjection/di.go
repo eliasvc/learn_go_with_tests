e708ffbc6eea7068735010ab0c8e0428e6becb25
Elias  Valdez <elias.vc@gmail.com>
2025-08-18 15:55:46 -0400

    Changed the package name to 'main' so Greet() can get used to print stuff to the screen. Also added a quick web server with http.ListenAndServe which uses Greet() too

--- dependencyinjection/di.go
+++ dependencyinjection/di.go
@@ -1,10 +1,22 @@
-package dependencyinjection
+package main
 
 import (
 	"fmt"
 	"io"
+	"log"
+	"net/http"
+	"os"
 )
 
 func Greet(writer io.Writer, name string) {
 	fmt.Fprintf(writer, "Hello, %s", name)
+}
+
+func MyGreeterHandler(w http.ResponseWriter, r *http.Request) {
+	Greet(w, "world!")
+}
+
+func main() {
+	Greet(os.Stdout, "Elodie")
+	log.Fatal(http.ListenAndServe("localhost:5001", http.HandlerFunc(MyGreeterHandler)))
 }
