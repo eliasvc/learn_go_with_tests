2a0d25584a1108963e7888183871066190baa4f6
Elias  Valdez <elias.vc@gmail.com>
2025-08-19 14:47:27 -0400

    Added a word and a countdown start constants and refactored Countdown() to use them

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -6,8 +6,14 @@ import (
 	"os"
 )
 
+const finalWord = "Go!"
+const countDownStart = 3
+
 func Countdown(writer io.Writer) {
-	fmt.Fprint(writer, "3")
+	for i := countDownStart; i > 0; i-- {
+		fmt.Fprintln(writer, i)
+	}
+	fmt.Fprint(writer, finalWord)
 }
 
 func main() {
--- mocking/countdown_test.go
+++ mocking/countdown_test.go
@@ -9,7 +9,10 @@ func TestCountdown(t *testing.T) {
 	buffer := &bytes.Buffer{}
 
 	Countdown(buffer)
-	want := "3"
+	want := `3
+2
+1
+Go!`
 	got := buffer.String()
 
 	if got != want {
