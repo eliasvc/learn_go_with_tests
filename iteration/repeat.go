ae7abf0c52228fb1839ec1bc1f0766884966f738
Elias Valdez <evaldez@linode.com>
2026-01-29 22:31:09 -0500

    Refactor Repeat to use a range over n loop and +=

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -2,8 +2,9 @@ package iteration
 
 func Repeat(char string, n int) string {
 	var repeated string
-	for i := 0; i < n; i++ {
-		repeated = repeated + char
+	for range n {
+		repeated += char
 	}
+
 	return repeated
 }
