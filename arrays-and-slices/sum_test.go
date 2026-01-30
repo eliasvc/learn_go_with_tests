e0ad7bfb04affc08fbf08d2a1bb1a66f161714dd
Elias Valdez <evaldez@linode.com>
2026-01-30 12:52:25 -0500

    Add SumAll, test and benchmark

--- arrays-and-slices/sum_test.go
+++ arrays-and-slices/sum_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("collection of any size", func(t *testing.T) {
@@ -13,4 +16,19 @@ func TestSum(t *testing.T) {
 			t.Errorf("got %d given %v, want %d", got, numbers, want)
 		}
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	got := SumAll([]int{1, 2, 3}, []int{1, 2, 3, 4, 5})
+	want := []int{6, 15}
+
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func BenchmarkSumAll(b *testing.B) {
+	for b.Loop() {
+		SumAll([]int{1, 2, 3}, []int{1, 2, 3, 4, 5})
+	}
 }
