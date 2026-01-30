a980c59ce3393d95dd0a1a840cd03efd19775960
Elias Valdez <evaldez@linode.com>
2026-01-29 22:51:53 -0500

    Add benchmark in test and improve efficiency in Repeat() by using strings.Builder

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,12 @@
 package iteration
 
+import "strings"
+
 func Repeat(char string, n int) string {
-	var repeated string
+	var repeated strings.Builder
 	for range n {
-		repeated += char
+		repeated.WriteString(char)
 	}
 
-	return repeated
+	return repeated.String()
 }
