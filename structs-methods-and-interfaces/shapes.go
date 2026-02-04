ed03eb4423c200d859e9613f0b373660665e434a
Elias Valdez <evaldez@linode.com>
2026-02-04 18:23:27 -0500

    Add perimeter method to triangle, circle and rectangle, and corresponding tests

--- structs-methods-and-interfaces/shapes.go
+++ structs-methods-and-interfaces/shapes.go
@@ -4,6 +4,7 @@ import "math"
 
 type Shape interface {
 	Area() float64
+	Perimeter() float64
 }
 
 type Rectangle struct {
@@ -15,6 +16,10 @@ func (r Rectangle) Area() float64 {
 	return r.Width + r.Height
 }
 
+func (r Rectangle) Perimeter() float64 {
+	return 2 * (r.Width + r.Height)
+}
+
 type Circle struct {
 	Radius float64
 }
@@ -23,13 +28,23 @@ func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
 }
 
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
 type Triangle struct {
 	Base   float64
 	Height float64
+	SideA  float64
+	SideB  float64
 }
 
 func (t Triangle) Area() float64 {
 	return (t.Base * t.Height) / 2
+}
+
+func (t Triangle) Perimeter() float64 {
+	return t.Base + t.SideA + t.SideB
 }
 
 func Perimeter(rectangle Rectangle) float64 {
--- structs-methods-and-interfaces/shapes_test.go
+++ structs-methods-and-interfaces/shapes_test.go
@@ -10,6 +10,24 @@ func TestPerimeter(t *testing.T) {
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
+
+	perimeterTests := []struct {
+		name         string
+		shape        Shape
+		hasPerimeter float64
+	}{
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6, SideA: 8, SideB: 18}, hasPerimeter: 38.0},
+		{name: "Rectangle", shape: Rectangle{Width: 10.0, Height: 10.0}, hasPerimeter: 40.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasPerimeter: 62.83185307179586},
+	}
+
+	for _, tt := range perimeterTests {
+		got := tt.shape.Perimeter()
+
+		if got != tt.hasPerimeter {
+			t.Errorf("got %g hasPerimeter %g", got, tt.hasPerimeter)
+		}
+	}
 }
 
 func TestArea(t *testing.T) {
