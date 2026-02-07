dcec403fc5e2a8a0f33cae8872cdf2e22e3bf2ba
Elias Valdez <evaldez@linode.com>
2026-02-06 20:21:17 -0500

    Add unknown word error, and get tests into their own runners

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,5 +1,16 @@
 package main
 
+import "errors"
+
+type Dictionary map[string]string
+
+func (d Dictionary) Search(word string) (string, error) {
+	if definition, ok := d[word]; ok {
+		return definition, nil
+	}
+	return "", errors.New("could not find the word you were looking for")
+}
+
 func Search(dictionary map[string]string, word string) string {
 	return dictionary[word]
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -3,11 +3,29 @@ package main
 import "testing"
 
 func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := Search(dictionary, "test")
-	want := "this is just a test"
-	if got != want {
-		t.Errorf("got %q, but want %q given %q", got, want, "test")
-	}
+	t.Run("word in dictionary", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		if got != want {
+			t.Errorf("got %q, but want %q given %q", got, want, "test")
+		}
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, err := dictionary.Search("unknown")
+		want := "could not find the word you were looking for"
+
+		if err == nil {
+			t.Fatal("Expected error but didn't get one")
+		}
+
+		if err.Error() != want {
+			t.Errorf("expected error %q, but got %q", want, err.Error())
+		}
+
+	})
+
 }
