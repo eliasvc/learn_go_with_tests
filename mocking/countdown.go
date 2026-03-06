caf4ca80b3ccb0d1052cf6f2f928f44bc4d9b84e
Elias Valdez <evaldez@linode.com>
2026-03-06 13:21:24 -0500

    Add Sleeper interface and use for Countdown. Also introduce SpySleeper for tests

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -10,14 +10,25 @@ import (
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(w io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(w io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintf(w, "%d\n", i)
-		time.Sleep(1 * time.Second)
+		sleeper.Sleep()
 	}
 	fmt.Fprintf(w, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	defaultSleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, defaultSleeper)
 }
