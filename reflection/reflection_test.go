89a86fada1fd33b633ff2c4f5497c5dc33664ebc
Elias Valdez <evaldez@linode.com>
2026-03-18 17:46:48 -0400

    Refactor tests to be table-based

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -1,20 +1,34 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalk(t *testing.T) {
-	expected := "Chris"
-	var got []string
+	cases := []struct {
+		Name          string
+		Input         any
+		ExpectedCalls []string
+	}{
+		{
+			"struct with one string field",
+			struct {
+				Name string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
+	}
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
+			var got []string
+			walk(test.Input, func(input string) {
+				got = append(got, input)
+			})
 
-	x := struct {
-		Name string
-	}{expected}
-
-	walk(x, func(input string) {
-		got = append(got, input)
-	})
-
-	if got[0] != expected {
-		t.Errorf("got %q, expected %q", got[0], expected)
+			if !reflect.DeepEqual(got, test.ExpectedCalls) {
+				t.Errorf("got %v, want %v", got, test.ExpectedCalls)
+			}
+		})
 	}
 }
