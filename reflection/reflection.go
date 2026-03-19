cc0b08fba5c605212786a08ad93c3d3f258bdd8a
Elias Valdez <evaldez@linode.com>
2026-03-19 15:01:38 -0400

    Add support for nested structs. Also refactor tests using a couple of new structs to make them more readable

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -7,8 +7,13 @@ func walk(x any, fn func(string)) {
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
+
 		if field.Kind() == reflect.String {
 			fn(field.String())
+		}
+
+		if field.Kind() == reflect.Struct {
+			walk(field.Interface(), fn)
 		}
 	}
 }
